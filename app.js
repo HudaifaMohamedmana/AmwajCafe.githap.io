3ede98d984a23b68c0905675c0468246f3946046
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-09 18:21:32 -0400

    ads2.0 with js

---
+++ app.js
@@ -0,0 +1,43 @@
+
+const list = document.querySelectorAll('.advisement img');
+let ADID =0;
+let x = 1;
+let intValio =null;
+intValioslider();
+
+
+function intValioslider() {
+    list[ADID].classList.add("displaySlide");
+    list[ADID+x].classList.add("displaySlide");
+    intValio = setInterval(next, 2000);
+}
+function showSlide(i){
+    
+    if (i>=list.length-1) {
+        ADID = 0
+        
+    } else if(i <0){
+        ADID = list.length -1 ;
+    }
+
+
+
+    list.forEach(list =>{
+       
+        list.classList.remove("displaySlide");
+    })
+    list[ADID].classList.add("displaySlide");
+    list[ADID+x].classList.add("displaySlide");
+
+}
+function next() {
+    ADID++;
+    showSlide(ADID);
+    
+}
+function back() {
+    clearInterval(intValio);
+    ADID--;
+    showSlide(ADID);
+    
+}
