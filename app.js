d8bb4a137e454e85ae82050cee58c91d311344b1
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-10 14:51:43 -0400

    another one

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 
-// const list = document.querySelectorAll('.advisement img');
-// let ADID =0;
-// let x = 1;
-// let intValio =null;
-// intValioslider();
+const list = document.querySelectorAll('.advisement img');
+let ADID =0;
+let x = 1;
+let intValio =null;
+intValioslider();
 
 
-// function intValioslider() {
-//     list[ADID].classList.add("displaySlide");
-//     list[ADID+x].classList.add("displaySlide");
-//     intValio = setInterval(next, 2000);
-// }
-// function showSlide(i){
+function intValioslider() {
+    list[ADID].classList.add("displaySlide");
+    list[ADID+x].classList.add("displaySlide");
+    intValio = setInterval(next, 2000);
+}
+function showSlide(i){
     
-//     if (i>=list.length-1) {
-//         ADID = 0;
-//         x=1;
-//     } else if(i <0){
-//         ADID = list.length -1 ;
-//     }
+    if (i>=list.length-1) {
+        ADID = 0;
+        x=1;
+    } else if(i <0){
+        ADID = list.length -1 ;
+    }
 
  
 
 
 
-//     list.forEach(list =>{
+    list.forEach(list =>{
        
-//         list.classList.remove("displaySlide");
-//     })
-//     list[ADID].classList.add("displaySlide");
-//     list[ADID+x].classList.add("displaySlide");
+        list.classList.remove("displaySlide");
+    })
+    list[ADID].classList.add("displaySlide");
+    list[ADID+x].classList.add("displaySlide");
 
-// }
-// function next() {
-//     ADID++;
-//     showSlide(ADID);
+}
+function next() {
+    ADID++;
+    showSlide(ADID);
     
-// }
-// function back() {
-//     clearInterval(intValio);
-//     ADID--;
-//     showSlide(ADID);
+}
+function back() {
+    clearInterval(intValio);
+    ADID--;
+    showSlide(ADID);
     
-// }
+}
 
 
 
