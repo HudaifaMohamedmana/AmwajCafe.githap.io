8dc772c5d9e5e39f947733a2366fed53257c48f8
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-10 14:25:37 -0400

    add to cart

--- app.js
+++ app.js
@@ -1,30 +1,64 @@
 
-const list = document.querySelectorAll('.advisement img');
-let ADID =0;
-let x = 1;
-let intValio =null;
-intValioslider();
+// const list = document.querySelectorAll('.advisement img');
+// let ADID =0;
+// let x = 1;
+// let intValio =null;
+// intValioslider();
 
 
-function intValioslider() {
-    list[ADID].classList.add("displaySlide");
-    list[ADID+x].classList.add("displaySlide");
-    intValio = setInterval(next, 2000);
-}
-function showSlide(i){
+// function intValioslider() {
+//     list[ADID].classList.add("displaySlide");
+//     list[ADID+x].classList.add("displaySlide");
+//     intValio = setInterval(next, 2000);
+// }
+// function showSlide(i){
     
-    if (i>=list.length-1) {
-        ADID = 0;
-        x=1;
-    } else if(i <0){
-        ADID = list.length -1 ;
-    }
-    list.forEach(list =>{
-       
-        list.classList.remove("displaySlide");
-    })
-    list[ADID].classList.add("displaySlide");
-    list[ADID+x].classList.add("displaySlide");
+//     if (i>=list.length-1) {
+//         ADID = 0;
+//         x=1;
+//     } else if(i <0){
+//         ADID = list.length -1 ;
+//     }
 
+ 
+
+
+
+//     list.forEach(list =>{
+       
+//         list.classList.remove("displaySlide");
+//     })
+//     list[ADID].classList.add("displaySlide");
+//     list[ADID+x].classList.add("displaySlide");
+
+// }
+// function next() {
+//     ADID++;
+//     showSlide(ADID);
+    
+// }
+// function back() {
+//     clearInterval(intValio);
+//     ADID--;
+//     showSlide(ADID);
+    
+// }
+
+
+
+// ====================================================================\\
+// ---------------------------------cart--------------------------------||
+//=====================================================================//
+
+
+
+const form = document.querySelector('form');
+
+ 
+function submitOrder(Event) {
+  console.log (Event) 
 }
+
+form.addEventListener("click",submitOrder);
+
 
